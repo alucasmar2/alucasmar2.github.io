c274402e9c96541cd313feaa3fc89e4f944ed7cc
alucasmar2 <lua_marinho2@hotmail.com>
2024-02-12 20:45:04 +0000

    Fisrt commit

---
+++ js/lightbox.js
@@ -0,0 +1,72 @@
+/*!
+ * Lightbox v
+ * by Lokesh Dhakar
+ *
+ * More info:
+ * http://lokeshdhakar.com/projects/lightbox2/
+ *
+ * Copyright Lokesh Dhakar
+ * Released under the MIT license
+ * https://github.com/lokesh/lightbox2/blob/master/LICENSE
+ *
+ * @preserve
+ */
+
+function registerKeyHandlers() {
+  document.addEventListener('keydown', event => {
+    const key = event.key;
+
+    if (key === 'ArrowLeft') {
+      // Handle left arrow press
+      plusSlides(-1);
+    } else if (key === 'ArrowRight') {
+      // Handle right arrow press
+      plusSlides(1);
+    } else if (key === 'Escape') {
+      // Handle escape key press
+      closeModal();
+    }
+  });
+}
+
+// Open the Modal
+function openModal() {
+  registerKeyHandlers();
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+//showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
